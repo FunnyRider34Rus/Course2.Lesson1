f7c1ae54f5299821b26412c28bc48d43f9aab426
elpablo <elpablo.volgograd@yandex.ru>
2023-08-28 19:32:42 +0300

    Initial Commit

---
+++ Course2.Lesson1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Course2.Lesson1
+//
+//  Created by Павел on 28.08.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
